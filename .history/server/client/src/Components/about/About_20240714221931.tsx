d3ba499e93cf90801a990aae7cd0926913fb9902
sana rizgar <sanarizgar34@gmail.com>
2024-07-15 22:36:29 +0300

    ....

--- .history/server/client/src/Components/about/About_20240714221931.tsx
+++ .history/server/client/src/Components/about/About_20240714221931.tsx
@@ -45,14 +45,14 @@ function About() {
           Download CV
         </a>
       </div>
-      {/* <motion.img
+      <motion.img
         className="profile-image"
         src="https://images.unsplash.com/photo-1622281587418-f2f4fc06ae7a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="person"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 3 }}
-      /> */}
+      />
     </div>
   );
 }
