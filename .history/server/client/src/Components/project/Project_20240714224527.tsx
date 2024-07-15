64a780538fe5369445d08bf053bc08b20afe5df6
sana rizgar <sanarizgar34@gmail.com>
2024-07-15 11:53:10 +0300

    ...

---
+++ .history/server/client/src/Components/project/Project_20240714224527.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRef } from 'react';
+import './project.scss';
+import { motion, useInView } from 'framer-motion';
+import ProjectDetail from '../ProjectDetail/ProjectDetail';
+import { useLoaderData } from 'react-router-dom';
+import { LoaderData } from '../../types.ts';
+function Project() {
+  const data = useLoaderData() as LoaderData | undefined;
+
+  return (
+    <div className="projects">
+      {data?.project?.data?.map((pro, index) => {
+        const ref = useRef(null);
+        const isInView = useInView(ref, { threshold: 0.1 });
+
+        return (
+          <motion.div
+            key={pro._id}
+            className="project"
+            ref={ref}
+            initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
+            animate={
+              isInView
+                ? { opacity: 1, x: 0 }
+                : { opacity: 0, x: index % 2 === 0 ? -100 : 100 }
+            }
+            transition={{ duration: 1.5, delay: index * 0.3 }}
+          >
+            <div
+              className={`${
+                index % 2 === 0 ? 'img-container' : 'project-detail'
+              }`}
+            >
+              {index % 2 === 0 ? (
+                <img src={pro.img} alt={pro.name} className="img" />
+              ) : (
+                <ProjectDetail isInView={isInView} pro={pro} />
+              )}
+            </div>
+            <div
+              className={`${
+                index % 2 === 0 ? 'project-detail' : 'img-container'
+              }`}
+            >
+              {index % 2 === 0 ? (
+                <ProjectDetail isInView={isInView} pro={pro} />
+              ) : (
+                <img src={pro.img} alt={pro.name} className="img" />
+              )}
+            </div>
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Project;
